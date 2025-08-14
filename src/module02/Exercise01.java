56a624acdf88d0092d60d0f2512f40214f7e5b7e
Raphael Rios <rios.raph@gmail.com>
2025-08-14 14:44:51 -0300

    Add Module 02: Exercise 01

--- src/module02/Exercise01.java
+++ src/module02/Exercise01.java
@@ -1,7 +1,17 @@
 package module02;
 
+import java.util.Scanner;
+
 public class Exercise01 {
     public static void main(String[] args) {
+        var scanner = new Scanner(System.in);
+        System.out.println("Informe um número:");
+        var number = scanner.nextInt();
+        System.out.println("Multiplication Table for " + number);
 
+        for (int i = 1; i <= 10; i++) {
+            var result = number * i;
+            System.out.println(number + " * " + i + " = " + result);
+        }
     }
 }
